12656f69a43339b0f0c7aee57c7ab13cb823a8ba
adityaDaripkar <52249417+adityaDaripkar@users.noreply.github.com>
2019-07-28 12:36:53 +0530

    Add files via upload

---
+++ transm.ino
@@ -0,0 +1,36 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+String valued;
+char text[9];
+int n;
+//blue, green, yellow;
+int j,i;
+//#include<string.h>
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+radio.begin();
+radio.openWritingPipe(address);
+radio.setPALevel(RF24_PA_MIN);
+radio.stopListening();
+}
+void loop() {
+while (Serial.available()>0)
+   {  
+  delay(10);  
+    char c = Serial.read(); 
+    if (c == '#') {break;} 
+    valued += c; 
+   // Serial.print(valued);
+   }
+   if(valued!="")
+   {
+    valued.toCharArray(text,10);
+    radio.write(&text, sizeof(text));
+   }
+   valued="";
+  
+}
