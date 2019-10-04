46ce858a791c1e6ead2ef85af62bd60d4e16ae05
adityaDaripkar <52249417+adityaDaripkar@users.noreply.github.com>
2019-10-04 16:49:14 +0530

    Update transm.ino

--- transm.ino
+++ transm.ino
@@ -1,10 +1,10 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(48, 49); // CE, CSN
 const byte address[6] = "00001";
 String valued;
-char text[9];
+char text[20];
 int n;
 //blue, green, yellow;
 int j,i;
